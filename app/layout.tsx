21b18ba49542ddd9e0f3dfa6f7ff2f6f04e62887
joeauscode <josephaustine045@gmail.com>
2025-09-03 04:42:20 -0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,10 +13,11 @@ export default function RootLayout({
       <body>
         <Header />
   
-
+ 
       {children}
+      <ChatBox />
        <Footer />
-       <ChatBox />
+      
       </body>
     </html>
   );
